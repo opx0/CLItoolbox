db36c4b2ec604dbfa5ca6b17452f17ea2dc077d2
Abhishek Yadav <akyv2.5@gmail.com>
2025-10-19 22:14:18 +0530

    scrennshot based on timestamp

--- grimShot.sh
+++ grimShot.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 dir="$HOME/Pictures"
-base="a"
+base="HBE"
 ext=".png"
 
-max_num=0
-for file in "$dir/${base}_"[0-9][0-9]"$ext"; do
-    if [ -f "$file" ]; then
-        num=$(basename "$file" "$ext" | sed "s/${base}_//")
-        if [ "$num" -gt "$max_num" ]; then
-            max_num=$num
-        fi
-    fi
+timestamp=$(date +%Y%m%d_%H%M%S)
+output_file="${dir}/${base}_${timestamp}${ext}"
+
+counter=1
+while [[ -f "$output_file" ]]; do
+    output_file="${dir}/${base}_${timestamp}_${counter}${ext}"
+    ((counter++))
 done
 
-next_num=$((max_num + 1))
-output_file="$dir/${base}_$(printf "%02d" $next_num)$ext"
-grim "$output_file"
+grim "$output_file" 2>/dev/null
